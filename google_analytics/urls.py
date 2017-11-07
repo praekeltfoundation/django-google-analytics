cb4670b6d7928e78e3b4b7c86bcda754de53f120
Codie Roelf <codie@praekelt.com>
2017-11-07 14:33:56 +0200

    remove trailing comma

--- google_analytics/urls.py
+++ google_analytics/urls.py
@@ -4,5 +4,5 @@ from google_analytics.views import google_analytics
 
 urlpatterns = [
     '',
-    url(r'^google-analytics/$', google_analytics, name='google-analytics', ),
+    url(r'^google-analytics/$', google_analytics, name='google-analytics'),
 ]
