f95174bd30d0c4088477b1939cc331650c489fb0
Codie Roelf <codie@praekelt.com>
2017-11-07 14:30:12 +0200

    import view because reversing with the dotted python path no longer works

--- google_analytics/urls.py
+++ google_analytics/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import url
+from google_analytics.views import google_analytics
+
 
 urlpatterns = [
     '',
-    url(
-        r'^google-analytics/$',
-        'google_analytics.views.google_analytics',
-        name='google-analytics'
-    ),
+    url(r'^google-analytics/$', google_analytics, name='google-analytics', ),
 ]
