f9d7b0709b62429b9fe0a8164fa1e61cc3bcb675
Anton Samarchyan <desecho@gmail.com>
2018-02-04 21:01:45 -0500

    Support django 2

--- google_analytics/templatetags/google_analytics_tags.py
+++ google_analytics/templatetags/google_analytics_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.conf import settings
 
 from google_analytics import CAMPAIGN_TRACKING_PARAMS
