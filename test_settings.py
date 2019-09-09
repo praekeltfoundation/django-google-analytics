6b78a5825f417dd300304e598c867bc951e9bd2b
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2019-09-09 17:20:04 +0200

    update test settings

--- test_settings.py
+++ test_settings.py
@@ -1,4 +1,9 @@
+import os
+
+
 SECRET_KEY = 'foo'
+BASE_DIR = os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 DATABASES = {
     'default': {
@@ -19,7 +24,25 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'jango.contrib.auth.middleware.AuthenticationMiddleware',
 ]
+
+TEMPLATES = [{
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [os.path.join(BASE_DIR, 'templates')],
+    'OPTIONS': {
+        'context_processors': [
+            'django.template.context_processors.debug',
+            'django.template.context_processors.request',
+            'django.contrib.auth.context_processors.auth',
+            'django.contrib.messages.context_processors.messages',
+            'molo.core.context_processors.locale',
+            'molo.core.processors.compress_settings',
+            'wagtail.contrib.settings.context_processors.settings',
+        ],
+    },
+}]
 
 STATIC_URL = ''
 SITE_ID = 1
