b14802dae4be6c048ccf6c202d251b4437bfa8d0
Wayne Lambert <wayne.a.lambert@gmail.com>
2020-08-13 16:44:19 +0100

    Update max version of Django to 3.2
    Updates oversight from previous commit so that the `install_requires`
    config in `setup.py` now has a max version for Django of 3.2
    
    Ref PR: #65

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     url='http://github.com/praekelt/django-google-analytics',
     packages=find_packages(),
     install_requires=[
-        'Django>=2.2.5,<3.1',
+        'Django>=2.2.5,<3.2',
         'django-celery',
         'celery<5.0',
         'requests',
