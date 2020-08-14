7a05c0a4485e51f615769933d4bac61bf12a769e
Wayne Lambert <wayne.a.lambert@gmail.com>
2020-08-14 14:53:25 +0100

    Change Celery requirement back to <4.0
    Making the package compatible with Celery >4.0 will take more extensive
    effort, therefore this has been reversed back to its prior state.
    
    The objective is to make the package compatible with Django >=3.1 and
    <3.2 with a view to addressing Celery compatibility at a later stage.
    
    Ref: #65
    See also: #64

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     install_requires=[
         'Django>=2.2.5,<3.2',
         'django-celery',
-        'celery<5.0',
+        'celery<4.0',
         'requests',
         'beautifulsoup4',
         'six',
