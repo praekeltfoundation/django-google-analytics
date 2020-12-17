b2633858dcee49248232d39980b350a79b03be86
Codie Roelf <codiebeulaine@gmail.com>
2020-12-17 08:24:18 +0200

    requirements update

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     install_requires=[
         'Django>=2.2.5,<3.2',
         'django-celery',
-        'celery<4.0',
+        'celery',
         'requests',
         'beautifulsoup4',
         'six',
