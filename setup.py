5cd77d282a59f74166f0bf38ff9120644b29c64b
Codie Roelf <codiebeulaine@gmail.com>
2020-12-17 14:18:46 +0200

    pin celery smaller than 5, 5 failing

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         'Django>=2.2.5,<3.2',
-        'celery',
+        'celery<5.0.0',
         'requests',
         'beautifulsoup4',
         'six',
