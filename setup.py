4523186abee4794ab828c9d7482ef1757a677383
Anton Samarchyan <desecho@gmail.com>
2021-04-10 13:49:57 -0400

    Support Django 3.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     url='http://github.com/praekelt/django-google-analytics',
     packages=find_packages(),
     install_requires=[
-        'Django>=2.2.5,<3.2',
+        'Django>=2.2.5,<3.3',
         'celery<5.0.0',
         'requests',
         'beautifulsoup4',
